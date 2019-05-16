b1ef04c6a6685a5d98b8b2c09e04e49b84bee71a
David Little <david.frank.little@gmail.com>
2019-05-16 13:42:27 -0400

    renamed variables & cleaner CI interface

--- src/online.jl
+++ src/online.jl
@@ -4,6 +4,7 @@ using Unitful
 using SampledSignals
 using LinearAlgebra
 using ProgressMeter
+using Distributions
 
 # TODO: preprcoessing, add lag and add intercept
 
@@ -143,106 +144,105 @@ Given two attention markers, x and y, use a batch, probabilistic state space
 model to compute a smoothed attentional state for x.
 """
 function attention(m1,m2;
-    outer_EM_batch = 20,
-    inner_EM_batch = 20,
-    Newton_iter = 10,
-    c0 = 1.65, # for #90 confidence intervals
+    outer_EM_iter = 20,
+    inner_EM_iter = 20,
+    newton_iter = 10,
+    ci = 0.9, # for #90 confidence intervals
 
     mean_p = 0.2,
     var_p = 5,
-    a_0 = 2+mean_p^2/var_p,
-    b_0 = mean_p*(a_0-1))
+    a₀ = 2+mean_p^2/var_p,
+    b₀ = mean_p*(a₀-1),
+
+    # tuned attended and unattended prior distributions based on a similar trial
+    α₀ = [5.7111e+02,1.7725e+03],
+    β₀ = [34.96324,2.1424e+02],
+    μ₀ = [0.88897,0.1619],
+
+    # initialized attended and unattended Log-Normal distribution parameters
+    # based on a similar trial
+    ρ_d₀ = [16.3344,8.2734],
+    μ_d₀ = [0.88897,0.16195])
 
     n = min(size(m1,1),size(m2,1))
 
     # Kalman filtering and smoothing variables
-    z_k = zeros(n+1,1)
-    z_k_1 = zeros(n+1,1)
-    sig_k = zeros(n+1,1)
-    sig_k_1 = zeros(n+1,1)
+    z_k = zeros(n+1)
+    z_k₁ = zeros(n+1)
+    σ_k = zeros(n+1)
+    σ_k₁ = zeros(n+1)
 
-    z_K = zeros(n+1,1)
-    sig_K = zeros(n+1,1)
+    z_K = zeros(n+1)
+    σ_K = zeros(n+1)
 
-    S = zeros(n,1)
+    S = zeros(n)
 
-    # tuned attended and unattended prior distributions based on a similar trial
-    alpha_0 = [5.7111e+02,1.7725e+03]
-    beta_0 = [34.96324,2.1424e+02]
-    mu_0 = [0.88897,0.1619]
+    ρ_d = ρ_d₀
+    μ_d = μ_d₀
 
-    # initialized attended and unattended Log-Normal distribution parameters
-    # based on a similar trial
-    rho_d_0 = [16.3344,8.2734]
-    mu_d_0 = [0.88897,0.16195]
-
-    rho_d = rho_d_0
-    mu_d = mu_d_0
-
-    # batch state-space model outputs for the two attentional markers
-    z_batch = zeros(n,1)
-    eta_batch = 0.3*ones(n,1)
+    # final results
+    z = zeros(n)
+    η = fill(0.3,n)
 
     # outer EM
-    for i in 1:outer_EM_batch
+    for i in 1:outer_EM_iter
         # calculating epsilon_k's in the current iteration (E-Step)
-                
+        P_11 = @. (1/m1)*sqrt(ρ_d[1])*exp(-0.5*ρ_d[1]*(log(m1)-μ_d[1])^2)
+        P_12 = @. (1/m1)*sqrt(ρ_d[2]).*exp(-0.5*ρ_d[2]*(log(m1)-μ_d[2])^2)
+        P_21 = @. (1/m2)*sqrt(ρ_d[2]).*exp(-0.5*ρ_d[2]*(log(m2)-μ_d[2])^2)
+        P_22 = @. (1/m2)*sqrt(ρ_d[1]).*exp(-0.5*ρ_d[1]*(log(m2)-μ_d[1])^2)
 
-        P_11 = @. (1/m1)*sqrt(rho_d[1])*exp(-0.5*rho_d[1]*(log(m1)-mu_d[1])^2)
-        P_12 = @. (1/m1)*sqrt(rho_d[2]).*exp(-0.5*rho_d[2]*(log(m1)-mu_d[2])^2)
-        P_21 = @. (1/m2)*sqrt(rho_d[2]).*exp(-0.5*rho_d[2]*(log(m2)-mu_d[2])^2)
-        P_22 = @. (1/m2)*sqrt(rho_d[1]).*exp(-0.5*rho_d[1]*(log(m2)-mu_d[1])^2)
-
-        p = @. (1/(1+exp(-z_batch)))
+        p = @. (1/(1+exp(-z)))
         
         E = @. (p*P_11*P_21)/(p*P_11*P_21+(1-p)*P_12*P_22)
                 
         # prior update
-        mu_d[1] = ( sum(E.*log.(m1).+(1.0.-E).*log.(m2)) + n*mu_0[1] )/(2*n)
-        mu_d[2] = ( sum(E.*log.(m2).+(1.0.-E).*log.(m1)) + n*mu_0[2] )/(2*n)
-        rho_d[1] = (2*n*alpha_0[1])/( sum( E.*((log.(m1).-mu_d[1]).^2) .+ (1.0.-E).*((log.(m2).-mu_d[1]).^2) ) + n*(2*beta_0[1]+(mu_d[1]-mu_0[1]).^2) )
-        rho_d[2] = (2*n*alpha_0[2])/( sum( E.*((log.(m2).-mu_d[2]).^2) .+ (1.0.-E).*((log.(m1).-mu_d[2]).^2) ) + n*(2*beta_0[2]+(mu_d[2]-mu_0[2]).^2) )
+        μ_d[1] = ( sum(E.*log.(m1).+(1.0.-E).*log.(m2)) + n*μ₀[1] )/(2*n)
+        μ_d[2] = ( sum(E.*log.(m2).+(1.0.-E).*log.(m1)) + n*μ₀[2] )/(2*n)
+        ρ_d[1] = (2*n*α₀[1])/( sum( E.*((log.(m1).-μ_d[1]).^2) .+ (1.0.-E).*((log.(m2).-μ_d[1]).^2) ) + n*(2*β₀[1]+(μ_d[1]-μ₀[1]).^2) )
+        ρ_d[2] = (2*n*α₀[2])/( sum( E.*((log.(m2).-μ_d[2]).^2) .+ (1.0.-E).*((log.(m1).-μ_d[2]).^2) ) + n*(2*β₀[2]+(μ_d[2]-μ₀[2]).^2) )
 
         # inner EM for updating z's and eta's (M-Step)
                 
-        for j in 1:inner_EM_batch
+        for j in 1:inner_EM_iter
 
             # Filtering
-            for kk in 2:n+1
-                z_k_1[kk] = z_k[kk-1]
-                sig_k_1[kk] = sig_k[kk-1] + eta_batch[kk-1]
+            for k in 2:n+1
+                z_k₁[k] = z_k[k-1]
+                σ_k₁[k] = σ_k[k-1] + η[k-1]
 
                 # Newton's Algorithm
-                for m in 1:Newton_iter
-                    z_k[kk] = z_k[kk] - (z_k[kk] - z_k_1[kk] - sig_k_1[kk]*(E[kk-1] - exp(z_k[kk])/(1+exp(z_k[kk])))) / (1 + sig_k_1[kk]*exp(z_k[kk])/((1+exp(z_k[kk]))^2))
+                for m in 1:newton_iter
+                    z_k[k] = z_k[k] - (z_k[k] - z_k₁[k] - σ_k₁[k]*(E[k-1] - exp(z_k[k])/(1+exp(z_k[k])))) / (1 + σ_k₁[k]*exp(z_k[k])/((1+exp(z_k[k]))^2))
                 end
 
-                sig_k[kk] = 1 / (1/sig_k_1[kk] + exp(z_k[kk])/((1+exp(z_k[kk]))^2))
+                σ_k[k] = 1 / (1/σ_k₁[k] + exp(z_k[k])/((1+exp(z_k[k]))^2))
             end
 
             # Smoothing
             z_K[n+1] = z_k[n+1]
-            sig_K[n+1] = sig_K[n+1]
+            σ_K[n+1] = σ_K[n+1]
 
-            for kk = n:-1:1
-                S[kk] = sig_k[kk]/sig_k_1[kk+1]
-                z_K[kk] = z_k[kk] + S[kk]*(z_K[kk+1] - z_k_1[kk+1])
-                sig_K[kk] = sig_k[kk] + S[kk]^2*(sig_K[kk+1] - sig_k_1[kk+1])
+            for k = reverse(1:n)
+                S[k] = σ_k[k]/σ_k₁[k+1]
+                z_K[k] = z_k[k] + S[k]*(z_K[k+1] - z_k₁[k+1])
+                σ_K[k] = σ_k[k] + S[k]^2*(σ_K[k+1] - σ_k₁[k+1])
 
             end
 
 
             z_k[1] = z_K[1]
-            sig_k[1] = sig_K[1]
+            σ_k[1] = σ_K[1]
 
             # update the eta's
-            eta_batch = @.( (z_K[2:end]-z_K[1:end-1])^2+sig_K[2:end]+sig_K[1:end-1]-2*sig_K[2:end]*S+2b_0 )/(1+2*(a_0+1))
+            η = @.( (z_K[2:end]-z_K[1:end-1])^2+σ_K[2:end]+σ_K[1:end-1]-2*σ_K[2:end]*S+2b₀ )/(1+2*(a₀+1))
 
         end
         
         # update the z's
-        z_batch = z_K[2:end]
+        z = z_K[2:end]
     end
 
-    z_batch, eta_batch
+    scale = cquantile(Normal(),0.5(1-ci))
+    z, z .+ scale.*sqrt.(η), z .- scale.*sqrt.(η)
 end
